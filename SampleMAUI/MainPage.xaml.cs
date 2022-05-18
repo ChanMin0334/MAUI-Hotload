d360d6919f5fa57e2c59330a390a0e7e0e489e6d
ChanMin0334 <52943035+ChanMin0334@users.noreply.github.com>
2022-05-18 11:40:18 +0900

    Upload

---
+++ SampleMAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleMAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SampleMAUI/MauiProgram.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using SampleMAUI.Data;
+using Windows.Graphics;
+
+namespace SampleMAUI;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+#if WINDOWS
+        builder.ConfigureLifecycleEvents(events =>
+        {
+            events.AddWindows(wndLifeCycleBuilder =>
+            {
+                wndLifeCycleBuilder.OnWindowCreated(window =>
+                {
+                    IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                    WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                    AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
+
+                    const int width = 1700;
+                    const int height = 1000;
+                    winuiAppWindow.MoveAndResize(new RectInt32(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height));
+                });
+            });
+        });
+#endif
+
+        return builder.Build();
+	}
+}
